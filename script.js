0734e79af56db1ebbe8c03ab327d474e5975febf
Esther Inyang <estherinyang07@gmail.com>
2022-03-08 19:09:09 +0100

    added changes to the menu

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-
+//ACTIVE MENU
 const nav = document.querySelector(".nav")
 const menuLinks = document.querySelectorAll(".menu-link")
 
@@ -10,7 +10,29 @@ menuLinks.forEach((menuLink) =>{
     })
 })
 
+//HAMBURGER FOR MOBILE
+const hamburgerOpen = document.querySelector(".hamburger-open")
+const hamburgerClose = document.querySelector(".hamburger-close")
 
+const heroTitle = document.querySelector(".hero-title")
+
+hamburgerOpen.addEventListener("click", ()=>{
+    hamburgerOpen.style.display = "none";
+    hamburgerClose.style.display = "block";
+    heroTitle.style.display = "none";
+    nav.classList.add("shownav")
+    nav.style.display = "block";
+})
+
+hamburgerClose.addEventListener("click", ()=>{
+    hamburgerClose.style.display = "none";
+    hamburgerOpen.style.display = "block";
+    heroTitle.style.display = "block";
+    nav.style.display = "none";
+    nav.classList.remove("shownav");
+})
+
+//BUTTONS
 const nextBtn = document.querySelector(".next-btn")
 const prevBtn = document.querySelector(".prev-btn")
 
@@ -18,28 +40,69 @@ const hero = document.querySelector(".hero")
 const articleTitle = document.querySelector(".shop-now-article h2")
 const articleText = document.querySelector(".shop-now-article p")
 
-counter = 0;
+let counter = 0;
 //Array for Sliders
-const heroSliders = [
+const pageSliders = [
     {
-        heroBg: url("./images/desktop-image-hero-1.jpg"), 
+        heroImgDesk: "url(./images/desktop-image-hero-1.jpg)",
+        heroImgMob: "url(./images/mobile-image-hero-1.jpg)",  
         articleTitle: "Discover innovative ways to decorate",
         articleText: "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.",
     },
 
     {
-        heroBg: url("./images/desktop-image-hero-2.jpg"), 
+        heroImgDesk: "url(./images/desktop-image-hero-2.jpg)", 
+        heroImgMob: "url(./images/mobile-image-hero-2.jpg)",  
         articleTitle: "We are available all across the globe",
         articleText: "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.",
     },
 
     {
-        heroBg: url("./images/desktop-image-hero-3.jpg"), 
+        heroImgDesk: "url(./images/desktop-image-hero-3.jpg)",
+        heroImgMob: "url(./images/mobile-image-hero-3.jpg)",  
         articleTitle: "Manufactured with the best materials",
         articleText: "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.",
     }
 ]
 
-nextBtn.addEventListener("click",()=>{
-    
+window.addEventListener("DOMContentLoaded", ()=>{
+    pageSlider()
+})
+
+function pageSlider(){
+    articleTitle.textContent = pageSliders[counter].articleTitle;
+    articleText.textContent = pageSliders[counter].articleText;
+
+    let screenWidth = screen.width;
+    // console.log(screenWidth)
+
+    if(screenWidth >= 992){
+        hero.style.backgroundImage = pageSliders[counter].heroImgDesk;
+    } else if(screenWidth >= 768){
+        hero.style.backgroundImage = pageSliders[counter].heroImgMob;
+    } else{
+        hero.style.backgroundImage = pageSliders[counter].heroImgMob;
+    }
+}
+
+nextBtn.addEventListener("click", ()=>{
+    counter++;
+
+    //if last slide
+    if(counter > pageSliders.length-1){
+        counter = 0;
+    }
+
+    pageSlider();
+})
+
+prevBtn.addEventListener("click", ()=>{
+    counter--;
+
+    //if last slide
+    if(counter < 0){
+        counter = pageSliders.length-1;
+    }
+
+    pageSlider();
 })
